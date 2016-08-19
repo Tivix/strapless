c1201710dc880105993e61fc3d79af4121780b3a
Rex Riepe <rex.riepe@tivix.com>
2016-08-19 16:01:21 -0700

    moved examples to homepage

--- strapless_web/strapless_web/apps/marketing/urls.py
+++ strapless_web/strapless_web/apps/marketing/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('marketing.views',
     url(r'^$', 'homepage', {}, name='homepage'),
-    url(r'^examples/$', 'examples', {}, name='examples'),
 )
