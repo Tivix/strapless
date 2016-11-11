d2348bc0a4bd2d0776ce7a240cb4e339b42a8177
Adam Lord <adam.lord@gmail.com>
2016-11-10 16:44:50 -0800

    Dynamic css link using base color. TODO: generate favicon on return

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 var path = require('path');
-
+var fs = require('fs');
 var express = require('express');
-var lessMiddleware = require('less-middleware');
-
+// var lessMiddleware = require('less-middleware');
+var less = require('less');
 var app = express();
 
-var html_dir = path.join(__dirname, 'public/');
+var html_dir = path.join(__dirname, 'source/');
 
-var baseColor = '';
+app.locals.baseColor = '003366';
 
-app.get('/:baseColor', function(req, res) {
-    baseColor = req.params.baseColor;
+app.get('/', function(req, res) {
+    // var baseColor = req.params.baseColor;
+    // if(!baseColor){
+    //     baseColor = app.locals.baseColor;
+    // }
     res.sendFile(html_dir + 'index.html');
 });
 
-app.use(lessMiddleware(path.join(__dirname, 'source'), {
-    dest: path.join(__dirname, 'public'),
-    preprocess: {
-        path: function(pathname, req) {
-            return pathname.replace('css', 'less');
-        },
-        less: function(src, req) {
-            var baseColorString = '@base-color: #' + baseColor + ';';
-            return baseColorString + src;
-        }
-    },
-    debug: true,
-    force: true
-}));
+app.get('/less/:baseColor?', function(req, res) {
+    var file_path = path.join(__dirname, 'source/less/index.less');
+    var baseColor = req.params.baseColor;
+    if(!baseColor){
+        baseColor = app.locals.baseColor;
+    }
+    fs.readFile(file_path, "utf8", function(err, data) {
+        if (err) throw err;
+        var options = {
+            filename: file_path,
+            sourceMap: {
+                sourceMapFileInline: true
+            }
+        };
+        var baseColorString = '@base-color: #' + baseColor + ';';
+        data = baseColorString + data;
+        less.render(data, options, function(error, output){
+            res.header("Content-type", "text/css");
+            res.send(output.css);
+        });
+    });
+});
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'public', 'static')));
 
 var server = app.listen('3000', function() {
     console.log('Listening on port %d', server.address().port);
