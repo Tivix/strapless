09cac5d0be8e6873b99c7b4d5459cc4e6f2b99f5
Adam Lord <adam.lord@gmail.com>
2016-11-11 16:33:16 -0800

    Reorganize files for Express version; ajax fetch new styles on form submit; color in url not including imports for some reason

--- index.js
+++ index.js
@@ -1,43 +1,78 @@
 var path = require('path');
 var fs = require('fs');
 var express = require('express');
-// var lessMiddleware = require('less-middleware');
+var exphbs = require('express-handlebars');
 var less = require('less');
 var app = express();
-
 var html_dir = path.join(__dirname, 'source/');
+var less_file_path = path.join(__dirname, 'source/less/index.less');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', html_dir);
 
 app.locals.baseColor = '003366';
 
-app.get('/', function(req, res) {
-    // var baseColor = req.params.baseColor;
-    // if(!baseColor){
-    //     baseColor = app.locals.baseColor;
-    // }
-    res.sendFile(html_dir + 'index.html');
-});
-
-app.get('/less/:baseColor?', function(req, res) {
-    var file_path = path.join(__dirname, 'source/less/index.less');
-    var baseColor = req.params.baseColor;
-    if(!baseColor){
-        baseColor = app.locals.baseColor;
-    }
-    fs.readFile(file_path, "utf8", function(err, data) {
-        if (err) throw err;
+var generateStyles = function(baseColor) {
+    console.log('generate styles');
+    return new Promise(function(resolve, reject) {
+        if (!baseColor) {
+            baseColor = app.locals.baseColor;
+        }
         var options = {
-            filename: file_path,
+            filename: less_file_path,
             sourceMap: {
                 sourceMapFileInline: true
             }
         };
-        var baseColorString = '@base-color: #' + baseColor + ';';
-        data = baseColorString + data;
-        less.render(data, options, function(error, output){
-            res.header("Content-type", "text/css");
-            res.send(output.css);
+        fs.readFile(less_file_path, "utf8", function(err, data) {
+            if (err) throw err;
+            var baseColorString = '@base-color: #' + baseColor + ';';
+            data = baseColorString + data;
+            var options = {
+                filename: less_file_path
+            };
+            console.log(options);
+            less.render(data, options)
+                .then(function(output) {
+                    resolve(output.css);
+                }, function(error) {
+                    throw err;
+                });
         });
     });
+}
+app.get('/:baseColor?', function(req, res) {
+    console.log('has base color?')
+    var baseColor = req.params.baseColor;
+    console.log(baseColor);
+    if (!baseColor) {
+        console.log('no base color')
+        res.render('index', {
+            styles: '',
+            static_css: true
+        });
+        // If there's no base color specified in the url,
+        // just send the static html pointing to the compiled css
+        // res.sendFile(path.join(__dirname, '/index.html'));
+    } else {
+        console.log('has base color')
+        // If the url includes a base color, generate the css,
+        // use the handlebars template, and render the css in the <style> tag
+        generateStyles(baseColor).then(function(css){
+            res.render('index', {
+                styles: css,
+                static_css: false
+            });
+        });
+    }
+});
+
+app.get('/less/:baseColor', function(req, res) {
+    console.log('ajax get');
+    generateStyles(req.params.baseColor).then(function(css){
+        res.send(css);
+    })
 });
 
 
--- public/static/js/strapless.js
+++ public/static/js/strapless.js
@@ -1,5 +1,6 @@
-(function() {
-    var generateFavicon = function() {
+var Strapless = (function() {
+
+    function _generateFavicon() {
         console.log('generate fav');
         var canvas = document.createElement('canvas');
         var ctx;
@@ -32,15 +33,87 @@
             link.href = canvas.toDataURL('image/png');
         }
     };
-    generateFavicon();
+
+    function _getLessFile(baseColor) {
+        return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/less/' + baseColor);
+            xhr.onload = function() {
+                if (this.status >= 200 && this.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                }
+            };
+            xhr.onerror = function() {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            };
+            xhr.send();
+        });
+    }
+
+    function getStyleSheet(unique_title) {
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            var sheet = document.styleSheets[i];
+            if (sheet.title == unique_title) {
+                return sheet;
+            }
+        }
+    }
+
+    function deleteStyleSheets() {
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            document.head.removeChild(document.styleSheets[i].ownerNode)
+        }
+    }
+
+    function _updateScheme() {
+        var styleBlock = document.getElementById('style_block');
+        var baseColor = document.getElementById('seed_color').value;
+        var overlay = document.getElementById('loading_overlay');
+        overlay.style.display = 'block';
+        _getLessFile(baseColor)
+            .then(function(result) {
+                deleteStyleSheets();
+                var css = result,
+                    head = document.head || document.getElementsByTagName('head')[0],
+                    style = document.createElement('style');
+
+                style.type = 'text/css';
+                if (style.styleSheet) {
+                    style.styleSheet.cssText = css;
+                } else {
+                    style.appendChild(document.createTextNode(css));
+                }
+
+                head.appendChild(style);
+                overlay.style.display = 'none';
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+    }
+
+    return {
+        updateScheme: function() {
+            _updateScheme();
+        },
+        generateFavicon: function() {
+            _generateFavicon();
+        }
+    };
 })();
 
-var updateScheme = function() {
-    var seed_color = document.getElementById('seed_color').value;
-    var less_link = document.getElementById('less_link');
-    var new_href = less_link.href.split('/').slice(0, 4).join('/') + '/' + seed_color;
-    less_link.href = new_href;
-}
+
+// var less_link = document.getElementById('less_link');
+// var new_href = less_link.href.split('/').slice(0, 4).join('/') + '/' + seed_color;
+// less_link.href = new_href;
 
 // var writeLessFiles = function() {
 //     return new Promise(function(resolve, reject) {
