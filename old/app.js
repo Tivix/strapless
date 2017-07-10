d246355f020bed0dfe4b3f05dd6bc0b437bf5366
Rex Riepe <rex.riepe@tivix.com>
2017-07-10 16:06:43 -0700

    added routes, better readability for patterns

--- old/app.js
+++ old/app.js
@@ -5,14 +5,36 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var less = require('less');
 var app = express();
-var html_dir = path.join(__dirname, 'source/');
+var html_dir = path.join(__dirname, 'views/');
 var demo_file_path = path.join(__dirname, 'source/less/index.less');
 var strapless_file_path = path.join(__dirname, 'source/less/strapless/strapless.less');
 var css_file_path = path.join(__dirname, 'source/less/strapless/strapless-css.less');
 
-app.engine('handlebars', exphbs());
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo: function () { return 'FOO!'; },
+        bar: function () { return 'BAR!'; }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res, next) {
+    res.render('home', {
+        showTitle: true,
+
+        // Override `foo` helper only for this rendering.
+        helpers: {
+            foo: function () { return 'foo.'; }
+        }
+    });
+});
+
 app.set('views', html_dir);
+
+
 
 var generateStyles = function(baseColor, filepath) {
     return new Promise(function(resolve, reject) {
@@ -48,26 +70,23 @@ app.use(function(req, res, next) {
 app.get('/:baseColor?', function(req, res) {
     var baseColor = req.params.baseColor;
     if (!baseColor) {
-        res.render('index', {
+        res.render('home', {
             styles: '',
             static_css: true
         });
-        // If there's no base color specified in the url,
-        // just send the static html pointing to the compiled css
-        // res.sendFile(path.join(__dirname, '/index.html'));
     } else {
         var validHex = /^(?:[0-9a-f]{3}){1,2}$/i.test(baseColor);
         if (validHex) {
             // If the url includes a base color, generate the css,
             // use the handlebars template, and render the css in the <style> tag
             generateStyles(baseColor, demo_file_path).then(function(css){
-                res.render('index', {
+                res.render('home', {
                     styles: css,
                     static_css: false
                 });
             });
         } else {
-            res.render('index', {
+            res.render('home', {
                 styles: '',
                 static_css: true,
                 invalid_hex: true
@@ -75,14 +94,6 @@ app.get('/:baseColor?', function(req, res) {
         }
     }
 });
-
-// app.get('/download/:baseColor', function(req, res) {
-//     generateStyles(req.params.baseColor, strapless_file_path).then(function(css){
-//         res.header("Content-Disposition", "attachment;filename=Library#" + req.params.baseColor + ".css");
-//         res.header("Content-type", "text/css");
-//         res.send(css);
-//     })
-// });
 
 app.get('/democss/:baseColor', function(req, res) {
     generateStyles(req.params.baseColor, demo_file_path).then(function(css){
